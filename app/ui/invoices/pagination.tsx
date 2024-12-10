0e0156e8f80638ca6650911eee74d42e7fd7c778
KathyNNguyen <kathynthi@outlook.com>
2024-12-10 15:10:05 -0500

    Revert "chapter 11 commit + bug fix"
    This reverts commit a4615192e7849b705e7631d40cbc3ffef1946743.

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -6,21 +6,15 @@ import Link from 'next/link';
 import { generatePagination } from '@/app/lib/utils';
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-  const currentPage = Number(searchParams.get('page')) || 1;
+  // NOTE: Uncomment this code in Chapter 11
 
-  const createPageURL = (pageNumber: number | string) => {
-    const params = new URLSearchParams(searchParams);
-    params.set('page', pageNumber.toString());
-    return `${pathname}?${params.toString()}`;
-  };
-
-  const allPages = generatePagination(currentPage, totalPages);
+  // const allPages = generatePagination(currentPage, totalPages);
 
   return (
     <>
-      <div className="inline-flex">
+      {/*  NOTE: Uncomment this code in Chapter 11 */}
+
+      {/* <div className="inline-flex">
         <PaginationArrow
           direction="left"
           href={createPageURL(currentPage - 1)}
@@ -53,7 +47,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
           href={createPageURL(currentPage + 1)}
           isDisabled={currentPage >= totalPages}
         />
-      </div>
+      </div> */}
     </>
   );
 }
