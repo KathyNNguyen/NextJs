100c4c045ea455635b8596eea9bedb335ed8d64f
KathyNNguyen <kathynthi@outlook.com>
2024-12-17 11:56:04 -0500

    adding new button for side nav

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -14,11 +14,12 @@ import clsx from 'clsx';
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
   {
-    name: 'Invoices',
+    name: 'Races',
     href: '/dashboard/invoices',
     icon: DocumentDuplicateIcon,
   },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'Teams', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'Login', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
 
 export default function NavLinks() {
