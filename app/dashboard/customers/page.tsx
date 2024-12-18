824769404225f8763e05519a3b63099d024cb5d2
KathyNNguyen <kathynthi@outlook.com>
2024-12-18 16:56:53 -0500

    adding card templates wip

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,241 @@
 export default function Page() {
-    return <p>Customer's Page</p>;
-  }
+  return (
+    <div className="h-full flex w-full justify-center items-center dark:bg-gray-800 p-2">
+      <div className="grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 p-4 md:p-2 xl:p-5">
+        {/* Card 1 */}
+        <div className="relative bg-white border rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 transform transition duration-500 hover:scale-105">
+          <div className="absolute top-3 right-3 rounded-full bg-violet-600 text-gray-200 w-6 h-6 text-center">24</div>
+          <div className="p-2 flex justify-center">
+            <a href="#">
+              <img
+                className="rounded-md"
+                src="https://tailwindflex.com/public/images/thumbnails/coming-soon-page/thumb_u.min.webp"
+                loading="lazy"
+                alt="Card 1 image"
+              />
+            </a>
+          </div>
+          <div className="px-4 pb-3">
+            <a href="#">
+              <h5 className="text-xl font-semibold tracking-tight hover:text-violet-800 dark:hover:text-violet-300 text-gray-900 dark:text-white">
+                Card
+              </h5>
+            </a>
+            <p className="antialiased text-gray-600 dark:text-gray-400 text-sm break-all">A card component</p>
+          </div>
+        </div>
+
+        {/* Card 2 */}
+        <div className="relative bg-white border rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 transform transition duration-500 hover:scale-105">
+          <div className="absolute top-3 right-3 rounded-full bg-violet-600 text-gray-200 w-6 h-6 text-center">19</div>
+          <div className="p-2 flex justify-center">
+            <a href="#">
+              <img
+                className="rounded-md"
+                src="https://tailwindflex.com/public/images/thumbnails/radio-buttons/thumb_u.min.webp"
+                loading="lazy"
+                alt="Card 2 image"
+              />
+            </a>
+          </div>
+          <div className="px-4 pb-3">
+            <a href="#">
+              <h5 className="text-xl font-semibold tracking-tight hover:text-violet-800 dark:hover:text-violet-300 text-gray-900 dark:text-white">
+                Input
+              </h5>
+            </a>
+            <p className="antialiased text-gray-600 dark:text-gray-400 text-sm break-all">description</p>
+          </div>
+        </div>
+
+        {/* Card 3 */}
+        <div className="relative bg-white border rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 transform transition duration-500 hover:scale-105">
+          <div className="absolute top-3 right-3 rounded-full bg-violet-600 text-gray-200 w-6 h-6 text-center">18</div>
+          <div className="p-2 flex justify-center">
+            <a href="#">
+              <img
+                className="rounded-md"
+                src="https://tailwindflex.com/public/images/thumbnails/buttons-with-border-bottom/thumb_u.min.webp"
+                loading="lazy"
+                alt="Card 3 image"
+              />
+            </a>
+          </div>
+          <div className="px-4 pb-3">
+            <a href="#">
+              <h5 className="text-xl font-semibold tracking-tight hover:text-violet-800 dark:hover:text-violet-300 text-gray-900 dark:text-white">
+                Button
+              </h5>
+            </a>
+            <p className="antialiased text-gray-600 dark:text-gray-400 text-sm break-all">description</p>
+          </div>
+        </div>
+
+                {/* Card 3 */}
+                <div className="relative bg-white border rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 transform transition duration-500 hover:scale-105">
+          <div className="absolute top-3 right-3 rounded-full bg-violet-600 text-gray-200 w-6 h-6 text-center">18</div>
+          <div className="p-2 flex justify-center">
+            <a href="#">
+              <img
+                className="rounded-md"
+                src="https://tailwindflex.com/public/images/thumbnails/buttons-with-border-bottom/thumb_u.min.webp"
+                loading="lazy"
+                alt="Card 3 image"
+              />
+            </a>
+          </div>
+          <div className="px-4 pb-3">
+            <a href="#">
+              <h5 className="text-xl font-semibold tracking-tight hover:text-violet-800 dark:hover:text-violet-300 text-gray-900 dark:text-white">
+                Button
+              </h5>
+            </a>
+            <p className="antialiased text-gray-600 dark:text-gray-400 text-sm break-all">description</p>
+          </div>
+        </div>
+
+                {/* Card 3 */}
+                <div className="relative bg-white border rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 transform transition duration-500 hover:scale-105">
+          <div className="absolute top-3 right-3 rounded-full bg-violet-600 text-gray-200 w-6 h-6 text-center">18</div>
+          <div className="p-2 flex justify-center">
+            <a href="#">
+              <img
+                className="rounded-md"
+                src="https://tailwindflex.com/public/images/thumbnails/buttons-with-border-bottom/thumb_u.min.webp"
+                loading="lazy"
+                alt="Card 3 image"
+              />
+            </a>
+          </div>
+          <div className="px-4 pb-3">
+            <a href="#">
+              <h5 className="text-xl font-semibold tracking-tight hover:text-violet-800 dark:hover:text-violet-300 text-gray-900 dark:text-white">
+                Button
+              </h5>
+            </a>
+            <p className="antialiased text-gray-600 dark:text-gray-400 text-sm break-all">description</p>
+          </div>
+        </div>
+
+                {/* Card 3 */}
+                <div className="relative bg-white border rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 transform transition duration-500 hover:scale-105">
+          <div className="absolute top-3 right-3 rounded-full bg-violet-600 text-gray-200 w-6 h-6 text-center">18</div>
+          <div className="p-2 flex justify-center">
+            <a href="#">
+              <img
+                className="rounded-md"
+                src="https://tailwindflex.com/public/images/thumbnails/buttons-with-border-bottom/thumb_u.min.webp"
+                loading="lazy"
+                alt="Card 3 image"
+              />
+            </a>
+          </div>
+          <div className="px-4 pb-3">
+            <a href="#">
+              <h5 className="text-xl font-semibold tracking-tight hover:text-violet-800 dark:hover:text-violet-300 text-gray-900 dark:text-white">
+                Button
+              </h5>
+            </a>
+            <p className="antialiased text-gray-600 dark:text-gray-400 text-sm break-all">description</p>
+          </div>
+        </div>
+
+                {/* Card 3 */}
+                <div className="relative bg-white border rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 transform transition duration-500 hover:scale-105">
+          <div className="absolute top-3 right-3 rounded-full bg-violet-600 text-gray-200 w-6 h-6 text-center">18</div>
+          <div className="p-2 flex justify-center">
+            <a href="#">
+              <img
+                className="rounded-md"
+                src="https://tailwindflex.com/public/images/thumbnails/buttons-with-border-bottom/thumb_u.min.webp"
+                loading="lazy"
+                alt="Card 3 image"
+              />
+            </a>
+          </div>
+          <div className="px-4 pb-3">
+            <a href="#">
+              <h5 className="text-xl font-semibold tracking-tight hover:text-violet-800 dark:hover:text-violet-300 text-gray-900 dark:text-white">
+                Button
+              </h5>
+            </a>
+            <p className="antialiased text-gray-600 dark:text-gray-400 text-sm break-all">description</p>
+          </div>
+        </div>
+
+                {/* Card 3 */}
+                <div className="relative bg-white border rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 transform transition duration-500 hover:scale-105">
+          <div className="absolute top-3 right-3 rounded-full bg-violet-600 text-gray-200 w-6 h-6 text-center">18</div>
+          <div className="p-2 flex justify-center">
+            <a href="#">
+              <img
+                className="rounded-md"
+                src="https://tailwindflex.com/public/images/thumbnails/buttons-with-border-bottom/thumb_u.min.webp"
+                loading="lazy"
+                alt="Card 3 image"
+              />
+            </a>
+          </div>
+          <div className="px-4 pb-3">
+            <a href="#">
+              <h5 className="text-xl font-semibold tracking-tight hover:text-violet-800 dark:hover:text-violet-300 text-gray-900 dark:text-white">
+                Button
+              </h5>
+            </a>
+            <p className="antialiased text-gray-600 dark:text-gray-400 text-sm break-all">description</p>
+          </div>
+        </div>
+
+                {/* Card 3 */}
+                <div className="relative bg-white border rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 transform transition duration-500 hover:scale-105">
+          <div className="absolute top-3 right-3 rounded-full bg-violet-600 text-gray-200 w-6 h-6 text-center">18</div>
+          <div className="p-2 flex justify-center">
+            <a href="#">
+              <img
+                className="rounded-md"
+                src="https://tailwindflex.com/public/images/thumbnails/buttons-with-border-bottom/thumb_u.min.webp"
+                loading="lazy"
+                alt="Card 3 image"
+              />
+            </a>
+          </div>
+          <div className="px-4 pb-3">
+            <a href="#">
+              <h5 className="text-xl font-semibold tracking-tight hover:text-violet-800 dark:hover:text-violet-300 text-gray-900 dark:text-white">
+                Button
+              </h5>
+            </a>
+            <p className="antialiased text-gray-600 dark:text-gray-400 text-sm break-all">description</p>
+          </div>
+        </div>
+
+        {/* Repeat Similar Structure for Remaining Cards */}
+        {/* Card 4 */}
+        <div className="relative bg-white border rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 transform transition duration-500 hover:scale-105">
+          <div className="absolute top-3 right-3 rounded-full bg-violet-600 text-gray-200 w-6 h-6 text-center">15</div>
+          <div className="p-2 flex justify-center">
+            <a href="https://tailwindflex.com/tag/form">
+              <img
+                className="rounded-md"
+                src="https://tailwindflex.com/public/images/thumbnails/sb-admin-2-login-page-with-tailwind/canvas.min.webp"
+                loading="lazy"
+                alt="Card 4 image"
+              />
+            </a>
+          </div>
+          <div className="px-4 pb-3">
+            <a href="https://tailwindflex.com/tag/form">
+              <h5 className="text-xl font-semibold tracking-tight hover:text-violet-800 dark:hover:text-violet-300 text-gray-900 dark:text-white">
+                Form
+              </h5>
+            </a>
+            <p className="antialiased text-gray-600 dark:text-gray-400 text-sm break-all">description</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+
